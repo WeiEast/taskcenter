dcda0d3b126554dc5c061aaaeffb287ba3c8014a
zhangyanjia <zhangyanjia@treefinance.com.cn>
2019-01-22 19:52:25 +0800

    修改

--- biz/src/main/java/com/treefinance/saas/taskcenter/biz/facade/impl/TaskPointFacadeImpl.java
+++ biz/src/main/java/com/treefinance/saas/taskcenter/biz/facade/impl/TaskPointFacadeImpl.java
@@ -13,6 +13,8 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -26,25 +28,48 @@ public class TaskPointFacadeImpl implements TaskPointFacade {
     @Autowired
     private TaskMapper taskMapper;
     @Autowired
-    private RedisDao redisDao;
+    private static RedisDao redisDao;
 
     @Override
     public TaskResult<Void> addTaskPoint(TaskPointRequest taskPointRequest) {
         TaskPoint taskPoint = new TaskPoint();
         BeanUtils.copyProperties(taskPointRequest, taskPoint);
-        String str=redisDao.get("UniqueId_"+taskPointRequest.getTaskId());
-        if (str==null) {
+        String str = redisDao.get("UniqueId_bizType_" + taskPointRequest.getTaskId());
+        if (str == null) {
             Task task = taskMapper.selectByPrimaryKey(taskPointRequest.getTaskId());
-            String taskId=task.getUniqueId();
-            redisDao.setEx("UniqueId_"+taskPointRequest.getTaskId(),task.getUniqueId(),10, TimeUnit.MINUTES);
+            String taskId = task.getUniqueId();
+            redisDao.setEx("UniqueId_bizType_" + taskPointRequest.getTaskId(), task.getUniqueId() + "," + task.getBizType(), 10, TimeUnit.MINUTES);
             taskPoint.setUniqueId(Long.parseLong(taskId));
-        }else{
-            taskPoint.setUniqueId(Long.parseLong(str));
+            taskPoint.setBizType(task.getBizType());
+        } else {
+            List<String> list = Arrays.asList(str.split(","));
+            taskPoint.setUniqueId(Long.parseLong(list.get(0)));
+            taskPoint.setBizType((Byte.valueOf(list.get(1))));
+        }
+        if ("crawler".equals(taskPoint.getStep())) {
+            int i = taskPoint.getBizType();
+            if (i == 1) {
+                taskPoint.setCode("2503");
+            } else if (i == 2) {
+                taskPoint.setCode("3404");
+            } else if (i == 3) {
+                taskPoint.setCode("1302");
+            }
+        }
+        if ("callback".equals(taskPoint.getStep())) {
+            int i = taskPoint.getBizType();
+            if (i == 1) {
+                taskPoint.setCode("2506");
+            } else if (i == 2) {
+                taskPoint.setCode("3409");
+            } else if (i == 3) {
+                taskPoint.setCode("1307");
+            }
         }
         taskPoint.setId(UidGenerator.getId());
-        int i=taskPointMapper.insertSelective(taskPoint);
+        int i = taskPointMapper.insertSelective(taskPoint);
         if (i == 1) {
-            //TODO 调功夫贷接口返回
+            // TODO 调功夫贷接口返回
         }
         return new TaskResult<>();
     }
