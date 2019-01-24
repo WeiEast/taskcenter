37597113e22165aed6885e1c1dddb00cacedc63e
guoguoyun <guoguoyun@treefinance.com.cn>
2019-01-24 17:00:50 +0800

    埋点枚举类更新

--- common/src/main/java/com/treefinance/saas/taskcenter/common/enums/CodeStepEnum.java
+++ common/src/main/java/com/treefinance/saas/taskcenter/common/enums/CodeStepEnum.java
@@ -30,11 +30,46 @@ public enum CodeStepEnum {
     OPERATOR_SUCCESS("10100401","importSuccess","success","点击“导入成功”按钮"),
 
     OPERATOR_LOGIN("10900101","login","login","运营商登录"),
-    OPERATOR_CRAWLER_START("10900201","crawler","stsrt","爬虫启动"),
+    OPERATOR_CRAWLER_START("10900201","crawler","start","爬虫启动"),
     OPERATOR_CRAWLER_SMS_VERIFY("10900202","crawler","sms_verify","触发短信验证"),
     OPERATOR_DATA_CLEAN("10900301","data","clean","数据清洗"),
     OPERATOR_DATA_STORAGE("10900302","data","storage","数据入库"),
-    OPERATOR_CALLBACK_NOTIFY("10900401","callback","notify","回调通知");
+    OPERATOR_CALLBACK_NOTIFY("10900401","callback","notify","回调通知"),
+
+
+    /**
+     * 电商
+     */
+    ECOMMERCE_CHOOSE_TAOBAO("30100101","loginBefore","start","选择淘宝。点击“淘宝”选项"),
+    ECOMMERCE_CHOOSE_ZHIFUBAO("30100102","loginBefore","start","选择支付宝。点击“支付宝”选项"),
+    ECOMMERCE_LOOK_AUTHORIZATION_SDK("30100103","loginBefore","start"," SDK点击查看个人信息查询授权书页面"),
+    ECOMMERCE_CANCEL_AUTHORIZATION("30100104","loginBefore","cancel_authorization","退出个人信息查询授权书页面"),
+    ECOMMERCE_LIST_CANCEL("30100105","loginBefore","exit_authorization_page","退出当前页面。点击左上角“<\"按钮"),
+    ECOMMERCE_THIRDPARTY_LOGIN_CANCEL("30100201","loginBefore","exit_authorization_page","退出当前第三方登录页面。点击左上角“<\"按钮"),
+    ECOMMERCE_AUTHORIZATION_QRCODE("30100301","loginBefore","get_qrcode","点击重新获得二维码"),
+    ECOMMERCE_AUTHORIZATION_("30100302","loginBefore","","点击手机淘宝一键认证"),
+    ECOMMERCE_LOOK_AUTHORIZATION_H5("30100303","loginBefore","look_authorization","H5点击查看个人信息查询授权书页面"),
+    ECOMMERCE_EXIT_AUTHORIZATION("30100304","loginBefore","exit_authorization","退出个人信息查询授权书页面。点击左上角“<\"按钮"),
+    ECOMMERCE_AUTHORIZATION_FAQ("30100305","loginBefore","faq","查看FAQ"),
+    ECOMMERCE_AUTHORIZATION_FEEDBACK("30100306","loginBefore","feedback","意见反馈"),
+    ECOMMERCE_AUTHENTICATION("30100401","import","authentication","支付宝安全验证,点击安全验证对话框的“确定”按钮"),
+    ECOMMERCE_BREAK("30100402","import","break","中断电商导入并退出当前页。退出对话框中，点击“确定退出”按钮"),
+    ECOMMERCE_SUCCESS("30100501","importSuccess","success","点击“导入成功”按钮"),
+    ECOMMERCE_EXIT("30100502","importSuccess","exit","退出当前页面。点击左上角“<\"按钮"),
+
+    ECOMMERCE_LOGIN("30900102","login","login","电商登录"),
+    ECOMMERCE_CRAWLER_START("30900201","crawler","start","爬虫启动"),
+    ECOMMERCE_CRAWLER_SMS_VERIFY("30900202","crawler","sms_verify","触发短信验证"),
+    ECOMMERCE_DATA_CLEAN("30900301","data","clean","数据清洗"),
+    ECOMMERCE_DATA_STORAGE("30900302","data","storage","数据入库"),
+    ECOMMERCE_CALLBACK_NOTIFY("30900401","callback","notify","回调通知");
+
+
+
+
+
+
+
     private String code;
 
     private String step;
