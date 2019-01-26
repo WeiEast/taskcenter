ac90ae82e4ead8c1e2a55b61f51319a54199316d
zhangyanjia <zhangyanjia@treefinance.com.cn>
2019-01-26 14:31:42 +0800

    日志

--- biz/src/main/java/com/treefinance/saas/taskcenter/facade/impl/TaskPointFacadeImpl.java
+++ biz/src/main/java/com/treefinance/saas/taskcenter/facade/impl/TaskPointFacadeImpl.java
@@ -1,6 +1,8 @@
-package com.treefinance.saas.taskcenter.biz.facade.impl;
+package com.treefinance.saas.taskcenter.facade.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.treefinance.saas.taskcenter.biz.service.TaskPointService;
+import com.treefinance.saas.taskcenter.context.component.AbstractFacade;
 import com.treefinance.saas.taskcenter.facade.request.TaskPointRequest;
 import com.treefinance.saas.taskcenter.facade.result.common.TaskResult;
 import com.treefinance.saas.taskcenter.facade.service.TaskPointFacade;
@@ -12,12 +14,13 @@ import org.springframework.stereotype.Service;
  * @since 2:45 PM 2019/1/22
  */
 @Service("taskPointFacade")
-public class TaskPointFacadeImpl implements TaskPointFacade {
+public class TaskPointFacadeImpl extends AbstractFacade implements TaskPointFacade {
     @Autowired
     private TaskPointService taskPointService;
 
     @Override
     public TaskResult<Void> addTaskPoint(TaskPointRequest taskPointRequest) {
+        logger.info("增加埋点请求参数 request={}", JSON.toJSONString(taskPointRequest));
         taskPointService.addTaskPoint(taskPointRequest);
         return new TaskResult<>();
     }
