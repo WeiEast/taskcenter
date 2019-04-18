2943337616ec3a8b0b00cd1fade2a1271b5a783d
Jerry Lee <amethystlilj@gmail.com>
2019-04-18 20:56:22 +0800

    优化TaskPointService

--- biz/src/main/java/com/treefinance/saas/taskcenter/biz/service/impl/TaskPointServiceImpl.java
+++ biz/src/main/java/com/treefinance/saas/taskcenter/biz/service/impl/TaskPointServiceImpl.java
@@ -1,26 +1,20 @@
 package com.treefinance.saas.taskcenter.biz.service.impl;
 
 import com.treefinance.commonservice.uid.UidService;
-import com.treefinance.saas.merchant.facade.request.console.MerchantFunctionRequest;
-import com.treefinance.saas.merchant.facade.result.console.MerchantFunctionResult;
-import com.treefinance.saas.merchant.facade.result.console.MerchantResult;
-import com.treefinance.saas.merchant.facade.service.MerchantFunctionFacade;
-import com.treefinance.saas.taskcenter.biz.domain.TaskPointInner;
 import com.treefinance.saas.taskcenter.biz.service.TaskPointService;
+import com.treefinance.saas.taskcenter.context.enums.ETaskAttribute;
 import com.treefinance.saas.taskcenter.dao.entity.Task;
 import com.treefinance.saas.taskcenter.dao.entity.TaskAttribute;
-import com.treefinance.saas.taskcenter.dao.entity.TaskAttributeCriteria;
 import com.treefinance.saas.taskcenter.dao.entity.TaskPoint;
-import com.treefinance.saas.taskcenter.dao.mapper.TaskAttributeMapper;
-import com.treefinance.saas.taskcenter.dao.mapper.TaskMapper;
 import com.treefinance.saas.taskcenter.dao.mapper.TaskPointMapper;
+import com.treefinance.saas.taskcenter.dao.repository.TaskAttributeRepository;
+import com.treefinance.saas.taskcenter.dao.repository.TaskRepository;
 import com.treefinance.saas.taskcenter.facade.request.TaskPointRequest;
 import com.treefinance.saas.taskcenter.facade.validate.Preconditions;
 import com.treefinance.saas.taskcenter.interation.manager.MerchantFunctionManager;
 import com.treefinance.saas.taskcenter.interation.manager.domain.MerchantFunctionBO;
 import com.treefinance.saas.taskcenter.share.cache.redis.RedisDao;
 import com.treefinance.saas.taskcenter.util.HttpClientUtils;
-import com.treefinance.toolkit.util.Objects;
 import com.treefinance.toolkit.util.net.NetUtils;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -30,10 +24,12 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Nonnull;
+
+import java.io.Serializable;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
@@ -52,15 +48,14 @@ public class TaskPointServiceImpl implements TaskPointService {
 
     private static final Logger logger = LoggerFactory.getLogger(TaskPointService.class);
 
-    private static final String sourceId = "sourceId";
     private static final int[] NOTIFY_BIZ_TYPE = {1, 2, 3};
 
     @Autowired
+    private TaskRepository taskRepository;
+    @Autowired
+    private TaskAttributeRepository taskAttributeRepository;
+    @Autowired
     private TaskPointMapper taskPointMapper;
-    @Autowired
-    private TaskAttributeMapper taskAttributeMapper;
-    @Autowired
-    private TaskMapper taskMapper;
     @Autowired
     private RedisDao redisDao;
     @Autowired
@@ -89,42 +84,17 @@ public class TaskPointServiceImpl implements TaskPointService {
         try {
             Preconditions.notNull("request", taskPointRequest);
             Preconditions.notBlank("request.code", taskPointRequest.getCode());
+
+            TaskPointInner info = this.readTaskPointInner(taskPointRequest.getTaskId());
+
             TaskPoint taskPoint = new TaskPoint();
-
             BeanUtils.copyProperties(taskPointRequest, taskPoint);
+            taskPoint.setId(uidService.getId());
             taskPoint.setOccurTime(new Date());
-            TaskPointInner str = (TaskPointInner)redisDao.getObject("UniqueId_bizType_appId_sourceId" + taskPointRequest.getTaskId());
-            String appId;
-            String sourceid = "";
-            if (Objects.isEmpty(str)) {
-                TaskPointInner taskPointInner = new TaskPointInner();
-                Task task = taskMapper.selectByPrimaryKey(taskPointRequest.getTaskId());
-                TaskAttributeCriteria criteria = new TaskAttributeCriteria();
-                criteria.createCriteria().andTaskIdEqualTo(taskPointRequest.getTaskId()).andNameEqualTo(sourceId);
-                List<TaskAttribute> taskAttributes = taskAttributeMapper.selectByExample(criteria);
-                if (!Objects.isEmpty(taskAttributes)) {
-                    sourceid = taskAttributes.get(0).getValue();
-                }
-                taskPointInner.setAppId(task.getAppId());
-                taskPointInner.setUniqueId(task.getUniqueId());
-                taskPointInner.setBizType(task.getBizType());
-                taskPointInner.setSourceId(sourceid);
+            taskPoint.setAppId(info.getAppId());
+            taskPoint.setUniqueId(info.getUniqueId());
+            taskPoint.setBizType(info.getBizType());
 
-                String uniqueId = task.getUniqueId();
-                redisDao.setObject("UniqueId_bizType_appId_sourceId" + taskPointRequest.getTaskId(), taskPointInner, (long)10, TimeUnit.MINUTES);
-
-                taskPoint.setUniqueId(uniqueId);
-
-                taskPoint.setBizType(task.getBizType());
-                appId = task.getAppId();
-            } else {
-                taskPoint.setUniqueId(str.getUniqueId());
-                taskPoint.setBizType(str.getBizType());
-                appId = str.getAppId();
-                if (StringUtils.isNotEmpty(str.getSourceId())) {
-                    sourceid = str.getSourceId();
-                }
-            }
             int bizType = taskPoint.getBizType();
             if (taskPoint.getType() == 1) {
                 taskPoint.setCode(createSystemTaskPointCode(bizType, taskPoint.getCode()));
@@ -132,18 +102,17 @@ public class TaskPointServiceImpl implements TaskPointService {
             taskPoint.setStep(getStep(taskPoint.getCode()));
             taskPoint.setSubStep(getSubStep(taskPoint.getCode()));
             taskPoint.setMsg(getMsg(taskPoint.getCode()));
-            taskPoint.setId(uidService.getId());
-            taskPoint.setAppId(appId);
+
             int i = taskPointMapper.insertSelective(taskPoint);
             if (i > 0 && supportNotifyBizType(bizType)) {
                 MerchantFunctionBO function = null;
                 try {
-                    function = merchantFunctionManager.getMerchantFunctionByAppId(appId);
+                    function = merchantFunctionManager.getMerchantFunctionByAppId(taskPoint.getAppId());
                 } catch (Exception e) {
                     logger.error("根据appId获取商户埋点信息同步配置失败!", e);
                 }
                 if (function != null) {
-                    notifyMerchant(function, taskPoint, appId, sourceid);
+                    notifyMerchant(function, taskPoint, StringUtils.defaultString(info.getSourceId()));
                 }
             }
         } catch (Exception e) {
@@ -151,21 +120,21 @@ public class TaskPointServiceImpl implements TaskPointService {
         }
     }
 
-    private void notifyMerchant(MerchantFunctionBO merchantFunction, TaskPoint taskPoint, String appId, String sourceId) {
+    private void notifyMerchant(MerchantFunctionBO merchantFunction, TaskPoint taskPoint, String sourceId) {
         if (merchantFunction.getSync() == 1) {
             logger.info("开始封装参数，taskId={}", taskPoint.getTaskId());
-            Map<String, Object> map = getStringObjectMap(taskPoint, appId, sourceId);
+            Map<String, Object> map = getStringObjectMap(taskPoint, sourceId);
             String result = HttpClientUtils.doPost(merchantFunction.getSyncUrl(), map);
             if (result == null) {
-                logger.error("埋点通知商户返回结果为空，taskId={},appId={}", taskPoint.getTaskId(), appId);
+                logger.error("埋点通知商户返回结果为空，taskId={},appId={}", taskPoint.getTaskId(), taskPoint.getAppId());
             } else {
                 logger.warn("埋点通知商户返回结果，taskId={}，result={}", taskPoint.getTaskId(), result);
             }
         }
     }
 
-    private Map<String, Object> getStringObjectMap(TaskPoint taskPoint, String appId, String sourceId) {
-        Map<String, Object> map = new HashMap<>(9);
+    private Map<String, Object> getStringObjectMap(TaskPoint taskPoint, String sourceId) {
+        Map<String, Object> map = new HashMap<>(11);
         map.put("taskId", taskPoint.getTaskId());
         map.put("uniqueId", taskPoint.getUniqueId());
         map.put("type", taskPoint.getType());
@@ -174,7 +143,7 @@ public class TaskPointServiceImpl implements TaskPointService {
         map.put("subStep", taskPoint.getSubStep());
         map.put("msg", taskPoint.getMsg());
         map.put("ip", taskPoint.getIp());
-        map.put("appId", appId);
+        map.put("appId", taskPoint.getAppId());
         map.put("sourceId", sourceId);
         DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.FULL, DateFormat.FULL);
         dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
@@ -191,5 +160,68 @@ public class TaskPointServiceImpl implements TaskPointService {
             }
         }
         return false;
+    }
+
+    private TaskPointInner readTaskPointInner(@Nonnull Long taskId) {
+        TaskPointInner taskPointInner = (TaskPointInner)redisDao.getObject("UniqueId_bizType_appId_sourceId:" + taskId);
+
+        if (taskPointInner == null) {
+            Task task = taskRepository.getTaskById(taskId);
+
+            String sourceId = null;
+            TaskAttribute attribute = taskAttributeRepository.queryAttributeByTaskIdAndName(taskId, ETaskAttribute.SOURCE_ID.getAttribute());
+            if (attribute != null) {
+                sourceId = attribute.getValue();
+            }
+
+            taskPointInner = new TaskPointInner();
+            taskPointInner.setAppId(task.getAppId());
+            taskPointInner.setUniqueId(task.getUniqueId());
+            taskPointInner.setBizType(task.getBizType());
+            taskPointInner.setSourceId(sourceId);
+
+            redisDao.setObject("UniqueId_bizType_appId_sourceId:" + taskId, taskPointInner, (long)10, TimeUnit.MINUTES);
+        }
+
+        return taskPointInner;
+    }
+
+    private static class TaskPointInner implements Serializable {
+        private String uniqueId;
+        private Byte bizType;
+        private String appId;
+        private String sourceId;
+
+        public String getUniqueId() {
+            return uniqueId;
+        }
+
+        public void setUniqueId(String uniqueId) {
+            this.uniqueId = uniqueId;
+        }
+
+        public Byte getBizType() {
+            return bizType;
+        }
+
+        public void setBizType(Byte bizType) {
+            this.bizType = bizType;
+        }
+
+        public String getAppId() {
+            return appId;
+        }
+
+        public void setAppId(String appId) {
+            this.appId = appId;
+        }
+
+        public String getSourceId() {
+            return sourceId;
+        }
+
+        public void setSourceId(String sourceId) {
+            this.sourceId = sourceId;
+        }
     }
 }
